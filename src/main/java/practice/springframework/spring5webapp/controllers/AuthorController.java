4476646636a00a0828485a913e7c60d44ddd1f8a
huyiqing1 <huyiqing1@gmail.com>
2022-04-02 00:21:44 -0400

    create authors template

---
+++ src/main/java/practice/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package practice.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import practice.springframework.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
